66d2699c67e0a0a64aa2d14308f765eea198bfb1
Max Schultz <mschultz@cochlear.com>
2020-11-01 12:27:19 +1100

    :lock: Updating strava pipeline to correctly handle API secrets

--- pipeline/etl.py
+++ pipeline/etl.py
@@ -4,14 +4,16 @@ Pipeline for ETL of fitness data
 '''
 
 # imports & modules
-
 import pandas as pd
 import datetime
 import os, sys
+import json
 
 import strava
+import settings
 
 # system globals
+settings.getEnv()
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(abs_path)
 
@@ -20,12 +22,20 @@ class FitnessHandler():
     ''' Wrapper for pipeline methods '''
 
     def __init__(self):
+        ''' globals for wrapper '''
+        with open(os.path.join('..', 'conf', 'endpoints.json')) as conf:
+            self.endpoint_conf = json.load(conf)
+        
         self.current_date = datetime.datetime.now()
 
     def getRunning(self):
         ''' Extracts running data from Strava '''
-        df_strava = strava.getStravaActivities()
+        df_strava = strava.getStravaActivities(self.endpoint_conf['STRAVA'])
         return df_strava
+
+    def getWeightlifting(self):
+        ''' Extracts weightlifting data from Google Sheets '''
+        return None
 
     def runPipeline(self):
         ''' Executes pipeline '''
@@ -36,6 +46,8 @@ class FitnessHandler():
 
 
 if __name__ == "__main__":
+
+    # run pipeline
     pipeline = FitnessHandler()
     df_fitnessData = pipeline.runPipeline()
     df_fitnessData.to_csv(
--- pipeline/strava.py
+++ pipeline/strava.py
@@ -8,46 +8,43 @@ import time
 import json
 import os, sys
 
+# globals
 abs_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(abs_path)
-
-conf_path = os.path.join(abs_path, '..', 'conf')
 resources_path = os.path.join(abs_path, '..', 'resources')
-
 
 def getStravaSecrets() :
     '''fn to return strava secrets'''
-    with open(os.path.join(conf_path, 'stravaSecrets.json')) as json_file:
-        strava_secrets = json.load(json_file)
-
-    strava_secrets["redirect_uri"] = 'http://localhost/exchange_token'
-    strava_secrets["scope"] = 'activity:read_all'
-    strava_secrets["response_type"] = 'code'
-        
+    strava_secrets = {
+        "strava_pw": os.getenv("STRAVA_PW"),
+        "strava_user" : os.getenv("STRAVA_USER"),
+        "strava_client_id": os.getenv("STRAVA_CLIENT_ID"),
+        "strava_client_secret": os.getenv("STRAVA_CLIENT_SECRET"),
+        "response_type": "code",
+        "scope" : "activity:read_all",
+        "redirect_uri" : "http://localhost/exchange_token"
+    }
     return strava_secrets
     
-    
-def completeStravaAuth(strava_secrets, auth_url) :
+def completeStravaAuth(strava_conf, strava_secrets, auth_url) :
     '''
     fn to complete strava auth flow
     : param strava_secrets: JSON object of all config
     : param strava_secrets: parameterised uri 
     : return resp_url:
-    NOTE: this is shockingly bad, and literally the worst way to do this possible
+    NOTE: this is shockingly bad, and literally the worst way to do this possible - please fix
     '''
     
-    # attempt to automate in selenium
-
+    # attempt to automate in selenium (PLEASE FIX)
     driver = webdriver.Chrome(executable_path=os.path.join(resources_path, 'chromedriver'))
     driver.get(auth_url)
 
 
     time.sleep(1)
     email_element = driver.find_element_by_name("email")
-    email_element.send_keys(strava_secrets['email'])
+    email_element.send_keys(strava_secrets['strava_user'])
 
     pw_element = driver.find_element_by_name("password")
-    pw_element.send_keys(strava_secrets['password'])
+    pw_element.send_keys(strava_secrets['strava_pw'])
 
     login_element = driver.find_element_by_id("login-button")
     login_element.click()
@@ -61,9 +58,9 @@ def completeStravaAuth(strava_secrets, auth_url) :
 
     # extract code from resp url
     code = resp_url.split("code=")[1].split("&")[0]
-    tok_url = 'https://www.strava.com/oauth/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_typeauthorization_code'.format(
-        client_id = strava_secrets['client_id'],
-        client_secret = strava_secrets['client_secret'],
+    tok_url = strava_conf['URL_TOKEN'].format(
+        client_id = strava_secrets['strava_client_id'],
+        client_secret = strava_secrets['strava_client_secret'],
         code = code
     )
     
@@ -77,42 +74,37 @@ def completeStravaAuth(strava_secrets, auth_url) :
     
     return f_access_token
     
-def getStravaActivities() :
-    '''fn to return dataframe of all strava activities'''
+def getStravaActivities(strava_conf) :
+    '''
+    fn to return dataframe of all strava activities
+    : param strava_conf: strava conf details
+    '''
     
     # globals
-    activites_url = "https://www.strava.com/api/v3/activities"
     strava_secrets = getStravaSecrets()
+
+    auth_url = strava_conf['URL_AUTH'].format(
+        client_id = strava_secrets['strava_client_id'],
+        response_type = strava_secrets['response_type'],
+        redirect_uri = strava_secrets['redirect_uri'],
+        scope = strava_secrets['scope']
+    )
     
-    if len(strava_secrets) > 0 :
-    
-        auth_url = 'http://www.strava.com/oauth/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}&scope={scope}'.format(
-            client_id = strava_secrets['client_id'],
-            response_type = strava_secrets['response_type'],
-            redirect_uri = strava_secrets['redirect_uri'],
-            scope = strava_secrets['scope']
-        )
-        
-        f_access_token = completeStravaAuth(strava_secrets, auth_url)
+    f_access_token = completeStravaAuth(strava_conf, strava_secrets, auth_url)
 
-        df_activities = pd.DataFrame()
-        page = 1
-                                            
-        while True:
+    df_activities = pd.DataFrame()
+    page = 1
 
-            # get page of activities from Strava
-            resp = r.get(activites_url + '?' + f_access_token + '&per_page=50' + '&page=' + str(page))
-            _dict = resp.json()
+    # paginate across Strava data                              
+    while True:
+        resp = r.get(strava_conf['URL_ACTIVITIES'] + '?' + f_access_token + '&per_page=50' + '&page=' + str(page))
+        _dict = resp.json()
 
-            if len(_dict) == 0 :
-                break
+        if len(_dict) == 0 :
+            break
 
-            else :
-                df_activities = df_activities.append(pd.DataFrame(_dict)) 
-                page += 1
+        else :
+            df_activities = df_activities.append(pd.DataFrame(_dict)) 
+            page += 1
 
-        return df_activities
-        
-        
-    else :
-        return pd.DataFrame()
+    return df_activities
